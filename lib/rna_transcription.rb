0b7514383aa9a3d45957aeaeb2d3dc715a40bdf9
Shubham Agarwal <Shubhamagarwal1616@gmail.com>
2019-01-10 00:19:12 +0530

    add test 2

--- lib/rna_transcription.rb
+++ lib/rna_transcription.rb
@@ -2,10 +2,19 @@ class RnaTranscription
 
   def self.complement_of_dna(str)
     l=str.length
+    str2=""
     i=0
     if l==0
       return ""
+    elsif
+      while i<l do
+        if str[i]=='C'
+          str2 = str2 + 'G'
+        end
+        i=i+1
+      end
     end
+    return str2
   end
 
 end
--- spec/rna_transcription_spec.rb
+++ spec/rna_transcription_spec.rb
@@ -1,11 +1,21 @@
 require "rna_transcription"
 
 describe RnaTranscription do
+
   describe ".complement_of_dna" do
+    
     context "empty rna sequence" do
       it "returns empty string" do
         expect(RnaTranscription.complement_of_dna("")).to eql("")
       end
     end
+
+    context "rna_complement_of_cytosine_is_guanine" do
+      it "returns C" do
+        expect(RnaTranscription.complement_of_dna("C")).to eql("G")
+      end
+    end
+
   end
+
 end
