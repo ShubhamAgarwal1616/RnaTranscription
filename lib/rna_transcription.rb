17c497d3317746b5a8620acea4a11a3f8eacfd9d
Shubham Agarwal <Shubhamagarwal1616@gmail.com>
2019-01-10 23:15:24 +0530

    format code properly

--- lib/rna_transcription.rb
+++ lib/rna_transcription.rb
@@ -1,26 +1,26 @@
 class RnaTranscription
 
   #function to implement complement of dna
-  def self.complement_of_dna(str)
-    l = str.length
-    str2 = ""
+  def self.complement_of_dna(string)
+    l = string.length
+    complement = ""
     i = 0
     if l == 0
       return ""
     elsif while i < l do
-            if str[i] == 'C'
-              str2 += 'G'
-            elsif str[i] == 'G'
-              str2 += 'C'
-            elsif str[i] == 'T'
-              str2 += 'A'
-            elsif str[i] == 'A'
-              str2 += 'U'
+            if string[i] == 'C'
+              complement += 'G'
+            elsif string[i] == 'G'
+              complement += 'C'
+            elsif string[i] == 'T'
+              complement += 'A'
+            elsif string[i] == 'A'
+              complement += 'U'
             end
             i += 1
           end
     end
-    return str2
+    return complement
   end
   # End of function complement of dna from sa 007
 end
