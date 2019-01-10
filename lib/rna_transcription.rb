3d566a1ebedfea63c097a89fb94a9595983e28e2
Shubham Agarwal <Shubhamagarwal1616@gmail.com>
2019-01-10 13:19:20 +0530

    Format code

--- lib/rna_transcription.rb
+++ lib/rna_transcription.rb
@@ -1,24 +1,23 @@
 class RnaTranscription
 
   def self.complement_of_dna(str)
-    l=str.length
-    str2=""
-    i=0
-    if l==0
+    l = str.length
+    str2 = ""
+    i = 0
+    if l == 0
       return ""
-    elsif
-      while i<l do
-        if str[i]=='C'
-          str2 = str2 + 'G'
-        elsif str[i]=='G'
-          str2 = str2 + 'C'
-        elsif str[i]=='T'
-          str2 = str2 + 'A'
-        elsif str[i]=='A'
-          str2 = str2 + 'U'
-        end
-        i=i+1
-      end
+    elsif while i < l do
+            if str[i] == 'C'
+              str2 += 'G'
+            elsif str[i] == 'G'
+              str2 += 'C'
+            elsif str[i] == 'T'
+              str2 += 'A'
+            elsif str[i] == 'A'
+              str2 += 'U'
+            end
+            i += 1
+          end
     end
     return str2
   end
